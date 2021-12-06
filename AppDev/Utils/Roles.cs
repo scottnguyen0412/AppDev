a45f9d572082c926faa35f99c36d80a871fd0d26
nguyentankhoi <scottnguyen0412@gmail.com>
2021-12-06 18:45:51 +0700

    create Utils folder to divide role

---
+++ AppDev/Utils/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppDev.Utils
+{
+    public class Roles
+    {
+        public const string Admin = "admin";
+        public const string Trainner = "trainner";
+        public const string Trainee = "trainee";
+        public const string TrainingStaff = "trainingStaff";
+    }
+}
