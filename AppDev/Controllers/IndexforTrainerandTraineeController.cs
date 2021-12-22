0732b1c161eba55776a3c921cda204e247802791
0BiSu0 <vinhtuonglephuc@gmail.com>
2021-12-22 12:09:38 +0700

    View Course Assigned of Trainer

--- AppDev/Controllers/IndexforTrainerandTraineeController.cs
+++ AppDev/Controllers/IndexforTrainerandTraineeController.cs
@@ -42,5 +42,40 @@ namespace AppDev.Controllers
             }
             return View(traineeInDb);
         }
+        [HttpPost]
+        [Authorize(Roles = Roles.Trainner)]
+        public ActionResult UpdateProfileOfTrainer(Trainner trainner)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(trainner);
+            }
+            var trainerInDb = _context.trainers.SingleOrDefault(t => t.Id == trainner.Id);
+            if (trainerInDb == null)
+            {
+                return HttpNotFound();
+            }
+            trainerInDb.FullName = trainner.FullName;
+            trainerInDb.Age = trainner.Age;
+            trainerInDb.Address = trainner.Address;
+            trainerInDb.Specialty = trainner.Specialty;
+
+            _context.SaveChanges();
+            return RedirectToAction("HomeOfTrainer", "");
+        }
+        [HttpGet]
+        [Authorize(Roles = Roles.Trainner)]
+        public ActionResult ViewCourse()
+        {
+            //get courseCategories
+            var courseCategoryInDB = _context.courseCategories.ToList();
+
+            var userId = User.Identity.GetUserId();
+            var courseInDB = _context.assignTrainerToCourses
+                                .Where(t => t.Trainer.TrainerId == userId)
+                                .Select(c => c.Course).ToList();
+            return View(courseInDB);
+
+        }
     }
-}
+    }
