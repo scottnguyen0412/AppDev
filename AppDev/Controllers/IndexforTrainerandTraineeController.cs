01b5cd70aea2fcf99e60184a5dd2cedf3836e7f3
0BiSu0 <vinhtuonglephuc@gmail.com>
2021-12-22 11:54:26 +0700

    Create Home of Trainer Controller

---
+++ AppDev/Controllers/IndexforTrainerandTraineeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppDev.Models;
+using AppDev.Utils;
+using Microsoft.AspNet.Identity;
+
+namespace AppDev.Controllers
+{
+    public class IndexforTrainerandTraineeController : Controller
+    {
+        // GET: IndexForTrainerAndTrainee
+        private ApplicationDbContext _context;
+
+        public IndexforTrainerandTraineeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: IndexforTrainerandTrainee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Authorize(Roles = Roles.Trainner)]
+        public ActionResult HomeOfTrainer()
+        {
+            var userId = User.Identity.GetUserId();
+            var trainerInDb = _context.trainers.SingleOrDefault(t => t.TrainerId == userId);
+            return View(trainerInDb);
+        }
+    }
+}
